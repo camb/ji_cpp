7066d7b111a43ceb03dfb3ab3b7a40093b121c94
camb <cameron.bec@gmail.com>
2016-04-15 13:15:38 -0700

    added hacky handling of null pointer for last name

--- 13.3.cpp
+++ 13.3.cpp
@@ -4,24 +4,38 @@
 
 using namespace std;
 
-void getName(string *p_fn, string *p_ln);
+string getName(string *p_fn, string *p_ln);
 
 int main ()
 {
     string fn = "kung";
     string ln = "fu";
-    getName(& fn, & ln);
+    string ln2;
+    string *p_null = NULL;
 
-    cout << "Hello " << fn << " " << ln << "!" << endl;
+    ln2 = getName(& fn, p_null);
 
+    if (p_null == NULL)
+    {
+        p_null = &ln2;
+    }
+
+    cout << "Hello " << fn << " " << *p_null << "!" << endl;
 
 }
 
-void getName(string *p_fn, string *p_ln)
+string getName(string *p_fn, string *p_ln)
 {
-    cout << "Current values: " << *p_fn << " " << *p_ln << endl;
+    // cout << "Current values: " << *p_fn << " " << *p_ln << endl;
     cout << "Enter your first name: ";
-    getline(cin, *p_fn, '\n');
-    cout << "Enter your last name: ";
-    getline(cin, *p_ln, '\n');
+    cin >> *p_fn;
+
+    string temp;
+    if (p_ln == NULL)
+    {
+        p_ln = &temp;
+        cout << "Enter your last name: ";
+        cin >> *p_ln;
+    }
+    return *p_ln;
 }
