be1c8673c74206787dc4cf1e09b07d802b33c560
Cam <cameron.bec@gmail.com>
2016-04-15 01:09:40 -0700

    adding 13 problems

---
+++ 13.3.cpp
@@ -0,0 +1,27 @@
+// Chapter 12 Practice Problem 3
+
+#include <iostream>
+
+using namespace std;
+
+void getName(string *p_fn, string *p_ln);
+
+int main ()
+{
+    string fn = "kung";
+    string ln = "fu";
+    getName(& fn, & ln);
+
+    cout << "Hello " << fn << " " << ln << "!" << endl;
+
+
+}
+
+void getName(string *p_fn, string *p_ln)
+{
+    cout << "Current values: " << *p_fn << " " << *p_ln << endl;
+    cout << "Enter your first name: ";
+    getline(cin, *p_fn, '\n');
+    cout << "Enter your last name: ";
+    getline(cin, *p_ln, '\n');
+}
