dadd0a6a2ebb22db3b24bcf47d98a54acd6bfc07
camb <cameron.bec@gmail.com>
2016-04-15 17:32:04 -0700

    stack address comparison

---
+++ 13.5.cpp
@@ -0,0 +1,21 @@
+// Chapter 12 Practice Problem 5
+
+#include <iostream>
+
+using namespace std;
+
+void displayStackAddresses();
+
+int main ()
+{
+	displayStackAddresses();
+}
+
+void displayStackAddresses()
+{
+	int int1, int2;
+	if ( &int1 > &int2 )
+		cout << "int1: " << &int1 << '\t' << "int2:" << &int2 << endl;
+	else if ( &int2 > &int1 )
+		cout << "int2:" << &int2 << '\t' << "int1: " << &int1 << endl;
+}
