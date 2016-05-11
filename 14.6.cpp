8725f1b340879ec00f0ffcd8229fa65a0a7646d3
camb <cameron.bec@gmail.com>
2016-05-10 23:45:57 -0700

    fixed UDLF testing, working out good maze creation

--- 14.6.cpp
+++ 14.6.cpp
@@ -14,7 +14,7 @@ struct mazeTile
 
 void displayMaze(mazeTile** maze, int width, int height);
 bool inBounds(int width, int height, int test_w, int test_h);
-void makePath(mazeTile** maze, int w, int h, int cur_x, int cur_y, bool hit_edge);
+int makePath(mazeTile** maze, int w, int h, int cur_x, int cur_y, bool hit_edge);
 
 int main ()
 {
@@ -43,43 +43,66 @@ int main ()
     {
         p_p_maze[i] = new mazeTile[width];
     }
-    
-    // Initialize the blank maze
+
+
+    // TODO makePath == 2 continue to fake paths else clear and redo maze
+    // Establish the true maze path
+    int a = 0, b = 0;
+    while (true)
+    {
+        // Initialize the blank maze
+        cout << "Clear and making maze\n";
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                p_p_maze[i][j].isPath = false;
+            }
+        }
+
+        int start_w = width / (rand() % 2 + 2);
+        int start_h = height / (rand() % 2 + 2);
+        p_p_maze[start_h][start_w].isPath = true;
+        // cout << "Start Width: " << start_w << " Height: " << start_h << endl;
+        a = makePath(p_p_maze, width, height, start_w, start_h, true);
+        b = makePath(p_p_maze, width, height, start_w, start_h, true);
+        if (a + b == 2)
+        {
+            break;   
+        }
+        // else
+        //     break;
+    }
+
+    cout << "Complete path:\n";
+    displayMaze(p_p_maze, width, height);
+
+    // Create false paths that terminate before board edges
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            p_p_maze[i][j].isPath = false;
+            if (p_p_maze[i][j].isPath == true)
+            {
+                // cout << "Attempting p_p_maze i: " << i << " j: " << j << endl;
+                makePath(p_p_maze, width, height, i, j, false);
+            }
         }
     }
-    displayMaze(p_p_maze, width, height);
 
-    // Establish the true maze path
-    cout << "The Maze:\n";
-    int start_w = width / (rand() % 2 + 2);
-    int start_h = height / (rand() % 2 + 2);
-    p_p_maze[start_h][start_w].isPath = true;
-    cout << "Start Width: " << start_w << " Height: " << start_h << endl;
-    makePath(p_p_maze, width, height, start_w, start_h, true);
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (p_p_maze[i][j].isPath == true)
+            {
+                // cout << "Attempting p_p_maze i: " << i << " j: " << j << endl;
+                makePath(p_p_maze, width, height, i, j, false);
+            }
+        }
+    }
+    cout << "Complete maze:\n";
     displayMaze(p_p_maze, width, height);
-    cout << "Complete path:\n";
-    makePath(p_p_maze, width, height, start_w, start_h, true);
-    displayMaze(p_p_maze, width, height);
-
-
-    // Create false paths that terminate before board edges
-    // for (int i = 0; i < height; i++)
-    // {
-    //     for (int j = 0; j < width; j++)
-    //     {
-    //         if (p_p_maze[i][j].isPath == true)
-    //         {
-    //             cout << "Attempting p_p_maze i: " << i << " j: " << j << endl;
-    //             makePath(p_p_maze, width, height, i, j, false);
-    //         }
-    //     }
-    // }
-    // displayMaze(p_p_maze, width, height);
 
     // TODO Some sort of logic/check to prevent the maze from walking in a spiral/deadend and trapping itself.
     // TODO display the true path
@@ -124,11 +147,12 @@ bool inBounds(int width, int height, int test_w, int test_h)
 }
 
 
-void makePath(mazeTile** maze, int w, int h, int cur_w, int cur_h, bool hit_edge)
+int makePath(mazeTile** maze, int w, int h, int cur_w, int cur_h, bool hit_edge)
 {
     // TODO Currently the tryUDLR are not at all being used because we recursively call the function before accumulating 4 tries. This is assuredly causing issues.
     bool tryU = false, tryD = false, tryL = false, tryR = false;
-    while(true)
+    bool cont = true;
+    while(cont)
     {
         int randtry = rand() % 4;
         switch (randtry)
@@ -142,11 +166,16 @@ void makePath(mazeTile** maze, int w, int h, int cur_w, int cur_h, bool hit_edge
                 maze[cur_h - 1][cur_w - 1].isPath == false)
             {
                 cur_h--;
-                cout << "Up! ";
+                cont = false;
+                // cout << "Up! ";
             }
             else
+            {
                 tryU = true;
+            }
+            
             break;
+
         case 1:
             if (inBounds(w, h, cur_w, cur_h + 1) &&
                 maze[cur_h + 1][cur_w].isPath == false &&
@@ -156,11 +185,16 @@ void makePath(mazeTile** maze, int w, int h, int cur_w, int cur_h, bool hit_edge
                 maze[cur_h + 1][cur_w - 1].isPath == false)
             {
                 cur_h++;
-                cout << "Down! ";
+                cont = false;
+                // cout << "Down! ";
             }
             else
+            {
                 tryD = true;
+            }
+
             break;
+
         case 2:
             if (inBounds(w, h, cur_w - 1, cur_h) &&
                 maze[cur_h][cur_w - 1].isPath == false &&
@@ -170,11 +204,16 @@ void makePath(mazeTile** maze, int w, int h, int cur_w, int cur_h, bool hit_edge
                 maze[cur_h - 1][cur_w - 1].isPath == false)
             {
                 cur_w--;
-                cout << "Left! ";
+                cont = false;
+                // cout << "Left! ";
             }
             else
+            {
                 tryL = true;
+            }
+
             break;
+
         case 3:
             if (inBounds(w, h, cur_w + 1, cur_h) &&
                 maze[cur_h][cur_w + 1].isPath == false &&
@@ -184,50 +223,57 @@ void makePath(mazeTile** maze, int w, int h, int cur_w, int cur_h, bool hit_edge
                 maze[cur_h - 1][cur_w + 1].isPath == false)
             {
                 cur_w++;
-                cout << "Right! ";
+                cont = false;
+                // cout << "Right! ";
             }
             else
+            {
                 tryR = true;
+            }
+
             break;
         }
 
-        // Don't draw if an edge on a non-edge terminal
-        if (hit_edge == false)
-        {
-            if (cur_h == 0 || cur_w == 0 || cur_w == w -1 || cur_h == h - 1)
-            {
-                cout << "Non-edge terminal!\n";
-                break;
-            }
-        }
-        else
-            maze[cur_h][cur_w].isPath = true;
-
-        // If all directions are exhausted, break
         if (tryU == true && tryD == true && tryL == true && tryR == true)
         {
-            cout << "No options left!\n";
-            break;
+            // cout << "No options left!\n";
+            return 0;
+            // break;
         }
-        // If the maze has hit another edge, break
-        if (hit_edge == true &&
-            (cur_h == 0 || cur_w == 0 || cur_w == w -1 || cur_h == h - 1))
+
+    }
+
+    // Don't draw if an edge on a non-edge terminal
+    if (hit_edge == false)
+    {
+        if (cur_h == 0 || cur_w == 0 || cur_w == w -1 || cur_h == h - 1)
         {
-            cout << "Reached the end!\n";
-            break;
+            // cout << "Non-edge terminal!\n";
+            return 0;
         }
-        // If we haven't hit an edge and should, recurse with hit_edge = true
-        else if (hit_edge == true)
-        {
-            makePath(maze, w, h, cur_w, cur_h, true);
-            break;
-        }
-        // If we haven't hit an edge and shouldn't, recurse with hit_edge = false
-        // (hit_edge == false)
-        else if (hit_edge == false)
-        {
-            makePath(maze, w, h, cur_w, cur_h, false);
-            break;
-        }
+    }
+    
+    maze[cur_h][cur_w].isPath = true;
+
+    // If the maze has hit another edge, break
+    if (hit_edge == true &&
+        (cur_h == 0 || cur_w == 0 || cur_w == w -1 || cur_h == h - 1))
+    {
+        // cout << "Reached the end!\n";
+        // Use special return code for edge hit validate that we have an exit.
+        return 1;
+    }
+    // If we haven't hit an edge and should, recurse with hit_edge = true
+    else if (hit_edge == true)
+    {
+        makePath(maze, w, h, cur_w, cur_h, true);
+        return 0;
+    }
+    // If we haven't hit an edge and shouldn't, recurse with hit_edge = false
+    // (hit_edge == false)
+    else if (hit_edge == false)
+    {
+        makePath(maze, w, h, cur_w, cur_h, false);
+        return 0;
     }
 }
