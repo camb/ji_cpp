c19100082a5f7837fe6a85675499a26efda1d379
camb <cameron.bec@gmail.com>
2016-05-12 00:59:46 -0700

    basic TODO outline

---
+++ 15.1.cpp
@@ -0,0 +1,18 @@
+// Chapter 15 Practice Problem 1
+// Write a program to remove an element from a linked list; the remove function should take just the element to be removed. Is this function easy to write--and will it be fast? Could this be made easier or faster by adding an additional pointer to the list?
+
+#include <iostream>
+
+using namespace std;
+
+// TODO Define the basic linked list struct
+// TODO Instantiate the remove element function that takes a struct pointer argument
+
+int main ()
+{
+    // TODO Create the initial linked list pointer
+    // TODO Define a few structs and link them together
+    // TODO Run the remove function on a specific struct
+}
+
+// TODO Define the remove element function that takes a struct pointer argument
