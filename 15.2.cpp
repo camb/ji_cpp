1ded2f510ad5f85b80ed964bff84a6dd8569c0a6
camb <cameron.bec@gmail.com>
2016-05-15 10:25:17 -0700

    basic program outline

---
+++ 15.2.cpp
@@ -0,0 +1,15 @@
+// Chapter 15 Practice Problem 2
+// Write a program that adds elements to a linked list in sorted order, rather than at the beginning
+
+#include <iostream>
+
+using namespace std;
+
+// void add node sorted function
+
+int main ()
+{
+    // TODO initialize sorted linked list
+    // TODO run function a few times with numbers to sort that will interact with the original numbers and eachother
+    // TODO p_ to NULL and free memory
+}
