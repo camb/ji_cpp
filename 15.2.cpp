1bac3f7f05dc4dae76b727aac3a6eac315f3f2e5
Cameron Beck <cameronb@ilm.com>
2016-05-17 13:14:16 -0700

    building out add sorted function

--- 15.2.cpp
+++ 15.2.cpp
@@ -17,29 +17,65 @@ void addSortedNode(node* &linked_list, int new_num);
 
 int main ()
 {
+    // Initialize sorted linked list
     node* p_first = new node;
     p_first->num = 16;
     p_first->prev = NULL;
-    node* temp = new node;
-    p_first->next = temp;
-    temp->num = 45;
-    temp->prev = p_first;
-    temp = new node;
-    temp->num = 99;
-    temp->prev = p_first->next;
-    temp->prev->next = temp;
-    temp->next = NULL;
+    node* p_temp = new node;
+    p_first->next = p_temp;
+    p_temp->num = 45;
+    p_temp->prev = p_first;
+    p_temp = new node;
+    p_temp->num = 99;
+    p_temp->prev = p_first->next;
+    p_temp->prev->next = p_temp;
+    p_temp->next = NULL;
 
-    // TODO initialize sorted linked list
     // TODO run function a few times with numbers to sort that will interact with the original numbers and eachother
-    // TODO p_ to NULL and free memory
+    p_temp = p_first;
+    while(p_temp != NULL)
+    {
+        cout << p_temp->num << endl;
+        p_temp = p_temp->next;
+    }
+
+    addSortedNode(p_first, 4);
+
+    p_temp = p_first;
+    while(p_temp != NULL)
+    {
+        cout << p_temp->num << endl;
+        p_temp = p_temp->next;
+    }
+
+    // Free memory
+    p_first = NULL;
+    p_temp = NULL;
+    delete p_first;
+    delete p_temp;
 }
 
 
 void addSortedNode(node* &linked_list, int new_num)
 {
     cout << "blah\n";
-    // TODO if num < first num, num is first
+    node* p_cur = linked_list;
+    node* p_temp = new node;
+    p_temp->num = new_num;
+
+    // If num < first num, num is first
+    if (new_num < linked_list->num)
+    {
+        p_temp->next = linked_list;
+        linked_list->prev = p_temp;
+        linked_list = p_temp;
+    }
+
+    // Loop through linked list to find where the new node will sit, break if hit end
+    while(p_temp->num >= p_cur && p_cur != NULL)
+    {
+
+    }
     // TODO while cur_p !+ NULL loop
     // TODO if num > cur_p, proceed to next item in linked list
     // TODO if reach NULL and no nums are greater, 
