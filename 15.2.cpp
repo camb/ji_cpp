ea2bf016d3d9761c6967e750b2b7798815378802
Cam <cameron.bec@gmail.com>
2016-05-16 19:12:13 -0700

    basic list setup

--- 15.2.cpp
+++ 15.2.cpp
@@ -5,11 +5,42 @@
 
 using namespace std;
 
+struct node
+{
+    int num;
+    node* next;
+    node* prev;
+};
+
 // void add node sorted function
+void addSortedNode(node* &linked_list, int new_num);
 
 int main ()
 {
+    node* p_first = new node;
+    p_first->num = 16;
+    p_first->prev = NULL;
+    node* temp = new node;
+    p_first->next = temp;
+    temp->num = 45;
+    temp->prev = p_first;
+    temp = new node;
+    temp->num = 99;
+    temp->prev = p_first->next;
+    temp->prev->next = temp;
+    temp->next = NULL;
+
     // TODO initialize sorted linked list
     // TODO run function a few times with numbers to sort that will interact with the original numbers and eachother
     // TODO p_ to NULL and free memory
+}
+
+
+void addSortedNode(node* &linked_list, int new_num)
+{
+    cout << "blah\n";
+    // TODO if num < first num, num is first
+    // TODO while cur_p !+ NULL loop
+    // TODO if num > cur_p, proceed to next item in linked list
+    // TODO if reach NULL and no nums are greater, 
 }
