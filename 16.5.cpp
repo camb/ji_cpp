5aba1c5febca89f438cf4301b9ec9435fe09d5f1
camb <cameron.bec@gmail.com>
2016-05-20 11:39:04 -0700

    defining base case

--- 16.5.cpp
+++ 16.5.cpp
@@ -7,12 +7,19 @@ using namespace std;
 
 void printTowers(int towers[4][3]);
 
-void solveHanoi(int towers[4][3], int target);
+int solveHanoi(int towers[4][3]);
 
 int main ()
 {
     // Multidimensional array for the towers that will hold disk width values. The array is capable of holding 4 disks and has 3 columns
     int towers[4][3];
+    // When printing it will be displayed as:
+    // [4,0][4,1][4,2]
+    // [3,0][3,1][3,2]
+    // [2,0][2,1][2,2]
+    // [1,0][1,1][1,2]
+    // [0,0][0,1][0,2]
+    // so that 0 functions as the 'bottom' of the tower but it is viewed upright
 
     // Set initial values to 0, ie. no disk
     for (int i = 0; i < 4; i++)
@@ -29,7 +36,7 @@ int main ()
         towers[i][0] =  4 - i;
     }
 
-    solveHanoi(towers, 0);
+    solveHanoi(towers);
 
     // TODO create a recursive function to move disks
 }
@@ -52,9 +59,14 @@ void printTowers(int towers[4][3])
     cout << endl;
 }
 
-void solveHanoi(int towers[4][3], int target)
+int solveHanoi(int towers[4][3])
 {
-    if target = 0;
+    // TODO move topmost value
+    for (int i = 4; i >= 0; i--)
+    {
+        towers[i][0];
+    }
+
     // TODO If value above current value you're trying to move, recurvively call solve hanoi on that tile
     // If space has tile on it, move tile from space
     // Else if all above values are 0, move tile to space
@@ -62,4 +74,14 @@ void solveHanoi(int towers[4][3], int target)
 
     // Display towers after each iteration to show steps.
     printTowers(towers);
+
+    // Base case that will exit recusion
+    if (towers[0][2] == 4 &&
+        towers[1][2] == 3 &&
+        towers[2][2] = 2 &&
+        towers[3][2] == 1)
+    {
+        cout << "The tower is solved!\n";
+        return 0;
+    }
 }
