6e4d35bba147db5bbd4bd10d07d021a5aaa11b01
camb <cameron.bec@gmail.com>
2016-05-24 11:12:46 -0700

    problem definition

---
+++ 17.1.cpp
@@ -0,0 +1,11 @@
+// Chapter 17 Practice Problem 1
+// Write a program to display the contents of a binary tree. Can you write a program that prints the nodes in a binary tree in sorted order? What about in reverse sorted order?
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	
+}
