a63c1fc66d5126c223bbd6977d3642f930fef31f
camb <cameron.bec@gmail.com>
2016-09-20 13:11:37 -0700

    basic calculator with header file

---
+++ 21.1.h
@@ -0,0 +1,12 @@
+// Chapter 21 Practice Problem 1
+// Write a program that contains the functions add, subtract, multiply and divide. Each of these functions should take two integers, and return the result of the operation. Create a small calculator in that uses these functions. Put the function declarations into a header file, but include the source code for these functions in your source file.
+
+#ifndef PP201_H
+#define PP201_H
+
+float add(float x, float y);
+float subtract(float x, float y);
+float multiply(float x, float y);
+float divide(float x, float y);
+
+#endif
